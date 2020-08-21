a90e0ad025cc983bef428afec1d44d68ebb0c660
Ganze ZHENG <1062168846@qq.com>
2020-08-21 18:19:16 +1000

    Made a new folder for implementing multiple screens

--- App.js
+++ App.js
@@ -1,67 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import Home from "./app/screens/Home";
 import { StyleSheet, Text, Image, View, Button, Alert } from "react-native";
 
 export default function App() {
   console.log("App Executed");
-
-  return (
-    <View style={{ flex: 1, flexDirection: "row" }}>
-      <View style={styles.avatarContainer}>
-        <Text>G'day, staying healthy?</Text>
-        <Image style={styles.avatar} source={require("./assets/avatar.png")} />
-        <Button
-          color="fuchsia"
-          title="Feed Avatar"
-          onPress={() => Alert.alert("Avatar:", "Thank you!")}
-        />
-        <StatusBar style="auto" />
-      </View>
-
-      <View style={styles.menuContainer}>
-        <Button
-          title="Quest"
-          onPress={() => console.log("Quest Button Pressed")}
-        />
-        <Button
-          title="Nutritional Tips"
-          onPress={() => console.log("Nutritional Tips Button Pressed")}
-        />
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-around",
-          }}
-        >
-          <Button
-            title="Socialise"
-            onPress={() => console.log("Socialise Button Pressed")}
-          />
-          <Button
-            title="Setting"
-            onPress={() => console.log("Setting Button Pressed")}
-          />
-        </View>
-      </View>
-    </View>
-  );
+  return <Home />;
 }
-
-const styles = StyleSheet.create({
-  avatarContainer: {
-    flex: 1.5,
-    backgroundColor: "#f0f8ff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  menuContainer: {
-    flex: 1,
-    backgroundColor: "#f0f8ff",
-    alignItems: "stretch",
-    justifyContent: "space-around",
-  },
-  avatar: {
-    width: 175,
-    height: 175,
-  },
-});
