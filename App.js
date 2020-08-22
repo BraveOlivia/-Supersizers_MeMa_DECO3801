291351525263f8d4221be5403d9438f8929c4503
UQ\s4397674 <d.gozali@uqconnect.edu.au>
2020-08-22 15:47:55 +1000

    This is a test commit console Dicky

--- App.js
+++ App.js
@@ -5,5 +5,6 @@ import { StyleSheet, Text, Image, View, Button, Alert } from "react-native";
 
 export default function App() {
   console.log("App Executed");
+  console.log("This is a test branch - Dicky");
   return <Home />;
 }
