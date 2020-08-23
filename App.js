f86e93870bc4b5b231441f0f333994a49980973b
Ganze ZHENG <1062168846@qq.com>
2020-08-23 13:29:51 +1000

    Modified UI of home page

--- App.js
+++ App.js
@@ -6,4 +6,22 @@ import { StyleSheet, Text, Image, View, Button, Alert } from "react-native";
 export default function App() {
   console.log("App Executed");
   return <Home />;
+  // Android does not support Alert.prompt
+  // Leave this feature for now
+  // <View style={styles.startContainer}>
+  //   <Button
+  //     color="fuchsia"
+  //     title="Start"
+  //     onPress={() => Alert.prompt("Enter names", "Please Enter your name")}
+  //   />
+  // </View>
 }
+
+const styles = StyleSheet.create({
+  startContainer: {
+    flex: 1,
+    backgroundColor: "#f0f8ff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -2,64 +2,91 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, Image, View, Button, Alert } from "react-native";
 
-export default function App() {
+function Home() {
   return (
-    <View style={{ flex: 1, flexDirection: "row" }}>
+    <View style={styles.container}>
       <View style={styles.avatarContainer}>
-        <Text>G'day, staying healthy?</Text>
+        <Text>[Happiness Bar]</Text>
+        <Text>[Status Bar]</Text>
         <Image style={styles.avatar} source={require("../assets/avatar.png")} />
-        <Button
+        <Text style={styles.avatarDialogue}>
+          [AvatarName]: G'day[UserName], staying healthy?
+        </Text>
+        {/* <Button
           color="fuchsia"
           title="Feed Avatar"
           onPress={() => Alert.alert("Avatar:", "Thank you!")}
-        />
+        /> */}
         <StatusBar style="auto" />
       </View>
 
       <View style={styles.menuContainer}>
         <Button
-          title="Quest"
-          onPress={() => console.log("Quest Button Pressed")}
+          title="Quests"
+          onPress={() => console.log("Quests Button Pressed")}
         />
+
+        <Button
+          title="Items"
+          onPress={() => console.log("Items Button Pressed")}
+        />
+
         <Button
           title="Nutritional Tips"
           onPress={() => console.log("Nutritional Tips Button Pressed")}
         />
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-around",
-          }}
-        >
-          <Button
-            title="Socialise"
-            onPress={() => console.log("Socialise Button Pressed")}
-          />
-          <Button
-            title="Setting"
-            onPress={() => console.log("Setting Button Pressed")}
-          />
-        </View>
+
+        <Button
+          title="Socialise"
+          onPress={() => console.log("Socialise Button Pressed")}
+        />
+
+        <Button
+          title="Setting"
+          onPress={() => console.log("Setting Button Pressed")}
+        />
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#f0f8ff",
+    flex: 1,
+    flexDirection: "row",
+    paddingTop: 10,
+  },
+
   avatarContainer: {
     flex: 1.5,
-    backgroundColor: "#f0f8ff",
     alignItems: "center",
     justifyContent: "center",
+    paddingTop: 50,
   },
+
   menuContainer: {
-    flex: 1,
-    backgroundColor: "#f0f8ff",
+    flex: 0.9,
     alignItems: "stretch",
     justifyContent: "space-around",
+    padding: 10,
   },
+
   avatar: {
-    width: 175,
-    height: 175,
+    width: 200,
+    height: 200,
+    margin: 5,
+  },
+
+  avatarDialogue: {
+    backgroundColor: "#dcdcdc",
+    borderWidth: 1,
+    borderRadius: 5,
+    width: 250,
+    padding: 10,
+    marginTop: "auto",
+    marginBottom: 10,
   },
 });
+
+export default Home;
