a61ade3be52bd81e750866b03ffee66a9d66adc5
Bhavita Kishanraj Bhooma <bhavitakbhooma@gmail.com>
2020-10-07 18:13:58 +1000

    Added background image and changed icon colour

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,55 +1,73 @@
 import React, { Component } from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 import { Platform, KeyboardAvoidingView, SafeAreaView } from "react-native";
 import { GiftedChat } from "react-native-gifted-chat";
 import { NavigationContainer } from "@react-navigation/native";
 import { APIKeys } from "../src/firebase/APIKeys";
 
-// export default class ChatScreen extends Component {
-//   state = {
-//     messages: [],
-//   };
+export default class ChatScreen extends Component {
+  state = {
+    messages: [],
+  };
 
-//   get user() {
-//     return {
-//       _id: Fire.uid,
-//       name: this.props.navigation.state.params.name,
-//     };
-//   }
+  get user() {
+    return {
+      _id: Fire.uid,
+      name: this.props.navigation.state.params.name,
+    };
+  }
 
-//   componentDidMount() {
-//     Fire.get((message) =>
-//       this.setState((previous) => ({
-//         messages: GiftedChat.append(previous.messages, message),
-//       }))
-//     );
-//   }
+  componentDidMount() {
+    Fire.get((message) =>
+      this.setState((previous) => ({
+        messages: GiftedChat.append(previous.messages, message),
+      }))
+    );
+  }
 
-//   componentWillMount() {
-//     Fire.off();
-//   }
+  componentWillMount() {
+    Fire.off();
+  }
 
-//   render() {
-//     const chat = (
-//       <GiftedChat
-//         messages={this.state.messages}
-//         onSend={Fire.send}
-//         user={this.user}
-//       />
-//     );
+  render() {
+    const chat = (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={Fire.send}
+        user={this.user}
+      />
+    );
 
-//     if (Platform.OS == "android") {
-//       return (
-//         <KeyboardAvoidingView
-//           style={{ flex: 1 }}
-//           behavior="padding"
-//           keyboardVerticalOffset={30}
-//           enabled
-//         >
-//           {chat}
-//         </KeyboardAvoidingView>
-//       );
-//     }
+    if (Platform.OS == "android") {
+      return (
+        <KeyboardAvoidingView
+          style={{ flex: 1 }}
+          behavior="padding"
+          keyboardVerticalOffset={30}
+          enabled
+        >
+          {chat}
+        </KeyboardAvoidingView>
+      );
+    }
 
-//     return <SafeAreaView style={{ flex: 1 }}>{chat}</SafeAreaView>;
-//   }
-// }
+    return <SafeAreaView style={{ flex: 1 }}>{chat}</SafeAreaView>;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 3,
+    flexDirection: "column",
+  },
+  backrgoundImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  text: {
+    color: "grey",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+});
--- components/QuestScreen.js
+++ components/QuestScreen.js
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
   Image,
   Alert,
+  ImageBackground,
 } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -337,88 +338,12 @@ export default class QuestScreen extends Component {
     readData();
     return (
       <View style={styles.MainContainer}>
-        <AppIndex />
-
-        {/* <View style={styles.Content}> */}
-        {/* <View style={styles.QuestButtons}>
-            <Button
-              title="Daily Quest"
-              onPress={() => console.log("hello Daily Quest")}
-            />
-            <Button
-              title="Weekly Quest"
-              onPress={() => console.log("Weekly Quest")}
-            />
-            <Button
-              title="Group Quest"
-              onPress={() => console.log("Group Quest")}
-            />
-          </View> */}
-
-        {/* <CardView style={styles.taskCard} cornerRadius={5}>
-            <View style={styles.taskDiv}>
-              <Text style={styles.taskTitle}>Get up at 8 am</Text>
-              <Text style={styles.rewardPoint}>
-                + 5
-                <FontAwesomeIcon icon={faSmile} size={21} color={"orange"} />
-              </Text>
-            </View>
-            <Text style={styles.taskbody}>Quest 1 to do</Text>
-            <Button
-              style={styles.taskToDo}
-              title="Complete"
-              onPress={() => console.log("Complete 1st request")}
-            />
-          </CardView>
-
-          <CardView style={styles.taskCard} cornerRadius={5}>
-            <View style={styles.taskDiv}>
-              <Text style={styles.taskTitle}>Walk 1000 meters</Text>
-              <Text style={styles.rewardPoint}>
-                + 5
-                <FontAwesomeIcon icon={faLeaf} size={21} color={"green"} />
-              </Text>
-            </View>
-            <Text style={styles.taskbody}>Quest 2 to do</Text>
-            <Button
-              style={styles.taskToDo}
-              title="Complete"
-              onPress={() =>
-                Alert.alert(
-                  "Task complete?",
-                  "Confirm and get rewards",
-                  [
-                    {
-                      text: "Cancel",
-                      onPress: () => console.log("click cancel"),
-                      // onPress: () => SaveJson.testJson("hello world"),
-                      style: "cancel",
-                    },
-                    {
-                      text: "OK",
-                      onPress: () => this.addCompletion(),
-                    },
-                  ],
-                  { cancelable: false }
-                )
-              }
-
-              // console.log("Complete 2nd request")}
-            />
-          </CardView> */}
-
-        {/* <CardView style={styles.taskCard} cornerRadius={5}>
-            <View style={styles.taskDiv}>
-              <Text style={styles.taskTitle}>Eat an apple a day</Text>
-              <Text style={styles.rewardPoint}>
-                + 3
-                <FontAwesomeIcon icon={faLeaf} size={21} color={"green"} />
-              </Text>
-            </View>
-            <Text style={styles.taskbody}>Quest completed</Text>
-            <Button style={styles.taskDone} title="Done" />
-          </CardView> */}
-        {/* </View> */}
+        <ImageBackground
+          source={require("../assets/BackgroundOrange.png")}
+          style={styles.backgroundImage}
+        >
+          <AppIndex />
+        </ImageBackground>
       </View>
     );
   }
@@ -430,6 +355,11 @@ const styles = StyleSheet.create({
     flexDirection: "column",
   },
 
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
   navBar: {
     flexDirection: "row",
     justifyContent: "space-evenly",
