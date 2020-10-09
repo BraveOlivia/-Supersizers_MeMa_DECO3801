a29d8907662cecaec6fc86ae4e66bb86894120e2
olivia <zhouxuanolivia@gmail.com>
2020-10-09 15:19:15 +1000

    fix chat screen crash bug and adjust advatar width on home screen

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -9,9 +9,6 @@ import { Fire } from "../src/firebase/APIKeys";
 // };
 
 export default class ChatScreen extends React.Component {
-  //   static navigationOptions = ({ navigation }) => ({
-  //     title: (navigation.state.params || {}).name || "Chat!",
-  //   });
   state = {
     messages: [],
   };
@@ -19,7 +16,7 @@ export default class ChatScreen extends React.Component {
   get user() {
     return {
       //   name: this.props.navigation.state.params.name,
-      _id: Fire.uid,
+      _id: Fire.shared.uid,
     };
   }
 
@@ -27,21 +24,21 @@ export default class ChatScreen extends React.Component {
     return (
       <GiftedChat
         messages={this.state.messages}
-        onSend={Fire.send}
+        onSend={Fire.shared.send}
         user={this.user}
       />
     );
   }
 
   componentDidMount() {
-    Fire.on((message) =>
+    Fire.shared.on((message) =>
       this.setState((previousState) => ({
         messages: GiftedChat.append(previousState.messages, message),
       }))
     );
   }
   componentWillUnmount() {
-    Fire.off();
+    Fire.shared.off();
   }
 }
 
